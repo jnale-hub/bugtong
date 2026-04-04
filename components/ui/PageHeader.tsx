f28ec0aaed39c7be04164f96efc74e9000dccd1f
John Mark Delima <delimajohnmark14@gmail.com>
2026-04-04 22:25:11 +0800

    Add PageHeader component for customizable header layout

---
+++ components/ui/PageHeader.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import { Text, View } from "react-native";
+
+type PageHeaderProps = {
+  title?: string;
+  left?: ReactNode;
+  center?: ReactNode;
+  right?: ReactNode;
+};
+
+export default function PageHeader({
+  title,
+  left,
+  center,
+  right,
+}: PageHeaderProps) {
+  return (
+    <View className="flex-row items-center justify-between">
+      <View className="flex-1 items-start">
+        {left ? (
+          left
+        ) : title ? (
+          <View className="bg-ink px-4 py-1.5 border-2 shadow-soft rounded-2xl -rotate-1">
+            <Text className="text-white font-bold text-lg font-mulish">
+              {title}
+            </Text>
+          </View>
+        ) : null}
+      </View>
+      <View className="flex-1 items-center">{center ? center : null}</View>
+      <View className="flex-1 items-end">{right ? right : null}</View>
+    </View>
+  );
+}
