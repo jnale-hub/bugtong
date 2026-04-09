6504076e3441a7ae34e2f285a56966d71d9a5c34
John Mark Delima <delimajohnmark14@gmail.com>
2026-04-09 17:10:20 +0800

    Add react-native-url-polyfill import for web compatibility

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 import { Platform } from "react-native";
+import "react-native-url-polyfill/auto";
 
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL!,
