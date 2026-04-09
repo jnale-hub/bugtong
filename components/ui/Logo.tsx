e2bb932f6b2bdf71298aed991abfbfbd042060a4
John Mark Delima <delimajohnmark14@gmail.com>
2026-04-09 12:18:33 +0800

    Refactor Logo component to enhance layout and styling for improved UI consistency

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,13 +1,20 @@
 import { Link } from "expo-router";
-import { Pressable, Text } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function Logo() {
   return (
     <Link href="/" asChild>
       <Pressable accessibilityRole="link" accessibilityLabel="Go to home">
-        <Text className={`text-lg font-bold`}>
-          BUGTONG
-        </Text>
+        <View className="flex-row items-center justify-center">
+          <View className="w-6 h-6 border-2 border-stone-900 bg-emerald-300 flex items-center justify-center rounded">
+            <Text className="font-serif sm:text-xl text-lg font-bold flex-row text-center my-auto -rotate-3">
+              B
+            </Text>
+          </View>
+          <Text className="text-lg sm:text-xl font-bold font-serif hidden sm:inline-block">
+            ugtong
+          </Text>
+        </View>
       </Pressable>
     </Link>
   );
