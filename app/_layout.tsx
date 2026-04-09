b14e76146ea8e754a607f70d45530b9870824800
John Mark Delima <delimajohnmark14@gmail.com>
2026-04-09 21:08:32 +0800

    Implement splash screen handling and font loading in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,30 @@
 import { toastConfig } from "@/components/ui/ToastConfig";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 import Toast from "react-native-toast-message";
 import "../global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "Montserrat-Regular": require("../assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    "Sansita-ExtraBoldItalic": require("../assets/fonts/Sansita-ExtraBoldItalic.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <>
       <Stack screenOptions={{ headerShown: false }} />
