f8b7b657b6afa6900c2aa7b3217886ad1a9b0312
John Mark Delima <delimajohnmark14@gmail.com>
2026-04-10 11:02:45 +0800

    Update button height

--- components/KeyButton.tsx
+++ components/KeyButton.tsx
@@ -7,8 +7,8 @@ type KeyButtonProps = {
 
 export default function KeyButton({ char, onClickAction }: KeyButtonProps) {
   const buttonClass =
-    "flex-1 h-10 sm:h-12 flex items-center justify-center pb-1 bg-stone-50 rounded active:translate-y-0.5 active:shadow-none transition-all shadow-sm";
-  const textClass = "text-lg sm:text-xl uppercase";
+    "flex-1 h-14 flex items-center justify-center pb-1 bg-stone-50 rounded active:translate-y-0.5 active:shadow-none transition-all shadow-sm";
+  const textClass = "text-lg sm:text-xl uppercase font-sans";
 
   return (
     <Pressable
